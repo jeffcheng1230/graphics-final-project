71f7591e482f93c02dbb4530a64a1be52898325c
Jeffrey Cheng <jeffreycheng1230@gmail.com>
2024-12-10 01:39:29 -0500

    test2

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@
  * handles window resizes.
  *
  */
-import { WebGLRenderer, PerspectiveCamera, Vector3, PlaneGeometry, MeshBasicMaterial, Mesh, DoubleSide } from 'three'
+import { WebGLRenderer, PerspectiveCamera, Vector3, PlaneGeometry, MeshBasicMaterial, Mesh, DoubleSide } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { SeedScene } from 'scenes';
 
